c098e330b160c705ff28f01dd0ce2186bef64973
Maria Clara Ferreira <maria.clara.ferreira1026@gmail.com>
2026-08-04 01:03:17 -0300

    configuração inicial do build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,16 @@ plugins {
 dependencies {
     testImplementation("junit:junit:4.13.2")
 
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2025.2.6.2")
         testFramework(TestFrameworkType.Platform)
     }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
